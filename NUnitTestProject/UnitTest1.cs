0a1ed924ab95d64561cdfa4e02e51ca96272986d
himanshunete <himanshuneteh@gmail.com>
2021-02-08 18:49:37 +0530

    [himanshunete]Add UseNUnitTestToCheckPasswordRule3

--- NUnitTestProject/UnitTest1.cs
+++ NUnitTestProject/UnitTest1.cs
@@ -41,9 +41,9 @@ namespace NUnitTestProject
         }
 
         [Test]
-        public void Given_PasswordRule2_Expecting_TrueResult()
+        public void Given_PasswordRule3_Expecting_TrueResult()
         {
-            bool result = program.PasswordRule2ValidationNunitTest();
+            bool result = program.PasswordRule3ValidationNunitTest();
             Assert.AreEqual(true, result);
         }
 
